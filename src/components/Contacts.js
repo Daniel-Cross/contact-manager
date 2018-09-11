8dcc006a7116c1087dd7cdd4a388b852b17bf509
Daniel-Cross <crossy1686@gmail.com>
2018-09-11 19:46:18 +0100

    added styling

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
 import { Consumer } from '../context';
+import '../styles/Contacts.css';
 
 class Contacts extends Component {
   render() {
@@ -9,14 +10,14 @@ class Contacts extends Component {
         {value => {
           const { contacts } = value;
           return (
-            <React.Fragment>
+            <div className="Contacts">
               <h1 className="display-4 mb-2">
                 <span className="text-danger">Contact</span> List
               </h1>
               {contacts.map(contact => (
                 <Contact key={contact.id} contact={contact} />
               ))}
-            </React.Fragment>
+            </div>
           );
         }}
       </Consumer>
