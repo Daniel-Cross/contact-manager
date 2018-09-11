158dcf462d23b65d8a2d055c2b02dc3ec45d3556
Daniel-Cross <crossy1686@gmail.com>
2018-09-11 16:32:02 +0100

    routing fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './Contacts';
 import Navbar from './Navbar';
 import About from './About';
